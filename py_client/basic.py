2658336521c58f2254a9fe29ca83abff4e8f029e
mohsenabedelaal <mohsen.abedelaal@gmail.com>
2022-05-30 14:46:21 +0300

    created an django inner app.

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,7 +1,8 @@
 import requests
 
-endpoint = "https://httpbin.org/anything"
+endpoint = "http://127.0.0.1:8000/api/"
 
 get_response = requests.get(endpoint,json={"query":"Hello World"}) # HTTP Request
-print(get_response.json()) # print raw text response 
+print(get_response.text) # print raw text response 
+print(get_response.status_code) # print status response 
 
