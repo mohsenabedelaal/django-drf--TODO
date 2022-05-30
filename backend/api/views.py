59d1558ffcfac4443de0ca0064c5b7ee187c2a84
mohsenabedelaal <mohsen.abedelaal@gmail.com>
2022-05-30 14:57:10 +0300

    Echo out the data in django.

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,16 @@
+import json
 from django.http import JsonResponse
 
 def api_home(request,*args,**kwargs):
+    # request -> HttpRequest -> Django
+    # print(dir(request))
+
+    body = request.body # byte string of JSON data
+    data = {}
+    try:
+        data = json.loads(body) # JSON Data -> Python dict
+    except:
+        pass
+    data['params'] = dict(request.GET)
+    print(data)
     return JsonResponse({"message":"Hi there,this is your Django API response !!"})
--- py_client/basic.py
+++ py_client/basic.py
@@ -2,7 +2,7 @@ import requests
 
 endpoint = "http://127.0.0.1:8000/api/"
 
-get_response = requests.get(endpoint,json={"query":"Hello World"}) # HTTP Request
+get_response = requests.get(endpoint,params={"abc":123},json={"query":"Hello World"}) # HTTP Request
 print(get_response.text) # print raw text response 
 print(get_response.status_code) # print status response 
 
